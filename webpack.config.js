26928c9ec0157e597e1a5328ff0f12441480c42e
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2022-11-27 05:28:14 +0300

    feat: настроить source-map

--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,12 @@ module.exports = {
     port: 8080,
     open: true
   },
+  devtool: 'eval-source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
+        use: ['babel-loader', 'source-map-loader'],
         exclude: '/node_modules/'
       },
       {
